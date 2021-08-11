3e2c27286db3e8d178c577147a2bb3a6cc238a27
Mason Spalding <masonspaldingfl@gmail.com>
2021-08-11 16:48:32 -0400

    Play 5 rounds with score keeping and prompt

--- game.js
+++ game.js
@@ -56,9 +56,45 @@ function playRound(playerSelection, computerSelection) {
     }
 
 }
-const playerSelection = "paper";
-const computerSelection = computerPlay();
 
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    let games = 1;
+    let playerScore = 0;
+    let computerScore = 0;
+    let message = "Please Choose Rock, Paper, or Scissors.";
+    let defaultmsg = "Testing";
+    
+
+    do {
+        let playerSelection = window.prompt(message, defaultmsg);
+        let computerSelection = computerPlay();
+        results = playRound(playerSelection, computerSelection);
+
+        if (results == "You Win. Scissors beats Paper!" || results == "You Win. Rock beats Scissors!" || results == "You Win. Paper beats Rock!") {
+            playerScore = playerScore + 1;
+            console.log(playRound(playerSelection, computerSelection));
+            console.log("You Win Round " + games + "!")
+            console.log("The score is: " + playerScore + " - " + computerScore);
+        }
+
+        else if (results == "You Lose. Scissors beats Paper!" || results == "You Lose. Rock beats Scissors!" || results == "You Lose. Paper beats Rock!"){
+            computerScore = computerScore + 1;
+            console.log(playRound(playerSelection, computerSelection));
+            console.log("You Lose Round " + games + "!");
+            console.log("The score is: " + playerScore + " - " + computerScore);
+        }
+
+        else {
+            console.log("You Tied. No Points Added.")
+            console.log("The score is: " + playerScore + " - " + computerScore);
+        }
+
+        games = games + 1;
+    }
+    while (games < 6)
+
+}
+
+game();
 
 
